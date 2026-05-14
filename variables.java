ebcb11897c06453b540924bb062dcb8e33ac461e
DhanalakshmiB-hub <dhanalakshmib07@gmail.com>
2026-05-14 20:50:55 +0530

    Java - Variables

---
+++ variables.java
@@ -0,0 +1,16 @@
+import java.lang.System;
+import java.util.Scanner;
+
+public class variables {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        {
+            int age = sc.nextInt();
+
+            System.out.println("Enter name: "+name);
+            System.out.println("Enter age: "+age);
+        }
+    }
+}
