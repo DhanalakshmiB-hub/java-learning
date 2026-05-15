4a18b5678d5cf5702655ec4df464c3926276d5dc
DhanalakshmiB-hub <dhanalakshmib07@gmail.com>
2026-05-15 12:30:03 +0530

    Rename variable to userinput

--- userinput.java
+++ userinput.java
@@ -1,7 +1,7 @@
 import java.lang.System;
 import java.util.Scanner;
 
-public class variables {
+public class userinput {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
